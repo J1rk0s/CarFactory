2ee06202d24b3ab006ad807f8ccc1b25d4182169
J1rk0s <J1rk0s>
2022-05-30 19:32:33 +0200

    Now better

--- CarFactory/Classes/Factory.cs
+++ CarFactory/Classes/Factory.cs
@@ -2,43 +2,56 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using Spectre.Console;
 
 namespace CarFactory.Classes {
     public static class Factory {
         private static readonly List<string> Models = new List<string>() {
-            "model x", "model y", "model 3"
+            "model x", "model s", "model 3"
         };
         
         public static void Menu() {
-            string input = AnsiConsole.Prompt(new SelectionPrompt<string>()
-                .Title("Select an option")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more options)")
-                .AddChoices(new[] 
-                    {"1) Model X", 
-                        "2) Model S", 
+            while (true)
+            {
+                string input = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("Select an option")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more options)")
+                    .AddChoices(new[]
+                        {"1) Model X",
+                        "2) Model S",
                         "3) Model 3",
-                        "4) Show last car",
-                        "5) Show current car",
-                        "6) Exit"}));
-            Console.WriteLine(input);
-            switch (input) {
-                case "1) Model X":
-                    Car c = MakeCar("Model X");
-                    CreatePage(c);
-                    ShowPage("negr.html");
-                    break;
-                case "2) Model S":
-                    break;
-                case "3) Model 3":
-                    break;
-                case "4) Show last car":
-                    break;
-                case "5) Show current car":
-                    break;
-                case "6) Exit":
-                    break;
+                        "4) Show current car",
+                        "5) Exit"}));
+                Console.WriteLine(input);
+                switch (input)
+                {
+                    case "1) Model X":
+                        CreatePage(MakeCar("Model X"));
+                        ShowPage("negr.html");
+                        Console.Clear();
+                        break;
+                    case "2) Model S":
+                        CreatePage(MakeCar("Model S"));
+                        ShowPage("negr.html");
+                        Console.Clear();
+                        break;
+                    case "3) Model 3":
+                        CreatePage(MakeCar("Model 3"));
+                        ShowPage("negr.html");
+                        Console.Clear();
+                        break;
+                    case "4) Show current car":
+                        ShowPage("negr.html");
+                        Console.Clear();
+                        break;
+                    case "5) Exit":
+                        Console.WriteLine("Exiting!\nGoodBye!");
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        return;
+                }
             }
         }
 
@@ -62,11 +75,21 @@ namespace CarFactory.Classes {
                 model = HelperClass.Input<string>("Enter model name: ");
             }
 
-            if (engineType.ToLower() != "electric" || engineType.ToLower() != "hybrid") {
+            if (engineType.ToLower().Trim() != "electric" && engineType.ToLower().Trim() != "hybrid") {
                 Console.WriteLine($"{engineType}: Invalid engine!\nPlease select another one instead\nAvailable types are: Hybrid, Electric\n");
                 engineType = HelperClass.Input<string>("Select engine type: ");
             }
-            
+
+            if (year < 1900 || year > new DateTime().Year) {
+                Console.WriteLine("Invalid date! Try again!");
+                year = HelperClass.Input<int>("Enter valid year: ");
+            }
+
+            if (price < 100000) {
+                Console.WriteLine("Invalid price! Try again!");
+                price = HelperClass.Input<int>("Enter valid price: ");
+            }
+
             return new Car {
                 EngineType = engineType,
                 Model = model,
@@ -76,14 +99,45 @@ namespace CarFactory.Classes {
             };
         }
 
-        public static void CreatePage(Car car) {
+        private static void CreatePage(Car car) {
+            string imgP = "";
+            switch (car.Model)
+            {
+                case "Model X":
+                    imgP = "../../Assets/Imgs/model_X.jpg";
+                    break;
+                case "Model S":
+                    imgP = "../../Assets/Imgs/model_S.jpg";
+                    break;
+                case "Model 3":
+                    imgP = "../../Assets/Imgs/model_3.jpeg";
+                    break;
+            }
             string html = $@"
 <html>
 <head>
-    <title>{car.Model}</title>
+    <title>Mega sus Car</title>
+    <link rel='stylesheet' href='../../Assets/style.css'>
 </head>
-<body>
-<h1>{car.EngineType}</h1>
+<body style='background-color:black;'>
+<h1>Specs</h1>
+<table>
+  <tr style:'color:white;'>
+    <th>Engine Type</th>
+    <th>Seat Count</th>
+    <th>Car model</th>
+    <th>Year</th>
+  </tr>
+  <tr style:'color:white;'>
+    <td>{car.EngineType}</td>
+    <td>{car.SeatCount}</td>
+    <td>{car.Model}</td>
+    <td>{car.Year}</td>
+  </tr>
+</table>
+<img src='{imgP}'></img>
+<p>______________________________</p>
+<b style:'color:white;'>price: {car.Price}$</b>
 </body>
 </html>";
             File.WriteAllText("negr.html", html);
